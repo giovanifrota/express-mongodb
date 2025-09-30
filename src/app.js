23dd115788252f942bc9ef86c9d21bd0ef6c3ca2
Giovani Frota <giovanifrota45@gmail.com>
2025-09-30 17:31:21 -0300

    CRUD node.js com express

---
+++ src/app.js
@@ -0,0 +1,47 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json())
+
+const livros = [
+    {
+        id: 1,
+        titulo: "O senhor dos Aneis"
+    },
+    {
+        id: 2,
+        titulo: "O hobbit"
+    }
+]
+
+function buscarLivros(id){
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    })
+}
+
+app.get("/", (req, res) => {
+    res.status(200).send("Curso de Node.JS");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+
+app.get("/livros/:id", (req, res) => {
+    const index = buscarLivros(req.params.id);
+    res.status(200).json(livros[index]);
+})
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso");
+})
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscarLivros(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+})
+
+export default app;
