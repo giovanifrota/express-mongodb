ad12b4f7aabb53177d9fe6f1ce8ccc08c2387b28
Giovani Frota <135627043+giovanifrota@users.noreply.github.com>
2025-11-14 10:08:36 -0300

    Controller Delete

--- src/Routes/livrosRoutes.js
+++ src/Routes/livrosRoutes.js
@@ -7,5 +7,6 @@ routes.get("/livros", livroController.listarLivros);
 routes.get("/livros/:id", livroController.listarLivroPorId);
 routes.post("/livros", livroController.cadastrarLivro);
 routes.put("/livros/:id", livroController.atualizarLivro);
+routes.delete("/livros/:id", livroController.excluirLivro);
 
 export default routes;
